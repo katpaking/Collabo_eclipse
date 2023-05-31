245f8ca9ca4d61ac14b4f1eccd53d476f726173e
oraman <oraman@naver.com>
2023-05-31 11:54:25 +0900

    first commit and push action

---
+++ Park/src/Ch01/Hello.java
@@ -0,0 +1,8 @@
+package Ch01;
+
+public class Hello {
+	public static void main(String[] args) {
+		System.out.println("hello01!");
+	}
+
+}
---
+++ Park/src/module-info.java
@@ -0,0 +1,9 @@
+/**
+ * 
+ */
+/**
+ * @author kosma
+ *
+ */
+module Park {
+}
